8ba4d4de43432626eb2bc947b126026403ed0d37
Ac1truB29 <137839168+Ac1truB29@users.noreply.github.com>
2023-06-29 11:40:48 -0600

    update on instance type

--- main.tf
+++ main.tf
@@ -21,7 +21,6 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
- 
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = false
@@ -35,7 +34,7 @@ module "vpc" {
 
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  instance_type          = var.instance_type
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
@@ -43,21 +42,6 @@ resource "aws_instance" "blog" {
     Name = "HelloWorld"
   }
 }
-
- # module "blog_sg" {
-  #  source  = "terraform-aws-modules/security-group/aws"
-   # version = "5.1.0"
-
-   # vpc_id = data.aws_vpc.default.id
-  #  name    = "blog_new"
-  
-   # ingress_rules       = ["http-80-tcp", "https-443"]
-  #  ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  #   egress_rules       = "-1" 
-  #   egress_cidr_blocks = ["0.0.0.0/0"]
-  # }
-
 
   
  module "blog_sg" {
