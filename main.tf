cd60de6c21420e42ba651bde573de165db9eb964
Ac1truB29 <137839168+Ac1truB29@users.noreply.github.com>
2023-06-29 10:06:50 -0600

    tshoting

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "aws_security_group" "blog" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "blog_http_in" {
-  type        = "ingress"
+  # type        = "ingress"
   from_port   = 80
   to_port     = 80
   protocol    = "tcp" 
@@ -61,7 +61,7 @@ resource "aws_vpc_security_group_ingress_rule" "blog_http_in" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "blog_https_in" {
-  type        = "ingress"
+  # type        = "ingress"
   from_port   = 443
   to_port     = 443
   protocol    = "tcp" 
@@ -70,12 +70,12 @@ resource "aws_vpc_security_group_ingress_rule" "blog_https_in" {
   security_group_id = aws_security_group.blog.id
 }
 
-# resource "aws_vpc_security_group_egress_rule" "blog_everything_out" {
+ resource "aws_vpc_security_group_egress_rule" "blog_everything_out" {
   # type        = "egress"
-  # from_port   = 0
-  # to_port     = 0
-  # protocol    = "-1" 
-  # cidr_blocks = ["0.0.0.0/0"]
+   from_port   = 0
+   to_port     = 0
+   protocol    = "-1" 
+   cidr_blocks = ["0.0.0.0/0"]
 
-  # security_group_id = aws_security_group.blog.id
-# }
+   security_group_id = aws_security_group.blog.id
+ }
