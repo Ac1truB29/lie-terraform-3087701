3c8bb6191c4989f8bb97f2a317160d23cdc6b97b
Ac1truB29 <137839168+Ac1truB29@users.noreply.github.com>
2023-06-29 10:41:13 -0600

    update

--- main.tf
+++ main.tf
@@ -29,19 +29,37 @@ resource "aws_instance" "blog" {
   }
 }
 
- module "blog_sg" {
-   source  = "terraform-aws-modules/security-group/aws"
-   version = "5.1.0"
-   name    = "blog_new"
+ # module "blog_sg" {
+   # source  = "terraform-aws-modules/security-group/aws"
+   # version = "5.1.0"
+   # name    = "blog_new"
 
-    vpc_id = data.aws_vpc.default.id
+   # vpc_id = data.aws_vpc.default.id
   
-   ingress_rules       = ["http-80-tcp", "https-443"]
-    ingress_cidr_blocks = ["0.0.0.0/0"]
+   # ingress_rules       = ["http-80-tcp", "https-443"]
+   # ingress_cidr_blocks = ["0.0.0.0/0"]
 
-    egress_rules       = ["all-all"]
-    egress_cidr_blocks = ["0.0.0.0/0"]
-  }
+   #  egress_rules       = ["all-all"]
+   #  egress_cidr_blocks = ["0.0.0.0/0"]
+  # }
+
+
+  
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
+
+  vpc_id  = data.aws_vpc.default.id
+  name    = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+}
+
+
+
+
 
 resource "aws_security_group" "blog" {
   name        = "blog"
