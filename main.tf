fa01fa8e90e53257fdb1d58d7f253f0db236d432
Ac1truB29 <137839168+Ac1truB29@users.noreply.github.com>
2023-06-29 06:29:11 -0600

    sg group changes

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
     Name = "HelloWorld"
@@ -43,7 +43,6 @@ module "blog_sg" {
 
   egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
-
 }
 
 resource "aws_security_group" "blog" {
