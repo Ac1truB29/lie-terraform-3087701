29c85c616d7a41c09c40ba2d6dd3e630be3ed0d9
Ac1truB29 <137839168+Ac1truB29@users.noreply.github.com>
2023-06-29 10:35:52 -0600

    Use module for security group

--- main.tf
+++ main.tf
@@ -29,19 +29,19 @@ resource "aws_instance" "blog" {
   }
 }
 
-# module "blog_sg" {
- #  source  = "terraform-aws-modules/security-group/aws"
-  # version = "5.1.0"
-  # name    = "blog_new"
+ module "blog_sg" {
+   source  = "terraform-aws-modules/security-group/aws"
+   version = "5.1.0"
+   name    = "blog_new"
 
-   # vpc_id = data.aws_vpc.default.id
+    vpc_id = data.aws_vpc.default.id
   
-  # ingress_rules       = ["http-80-tcp", "https-443"]
-   # ingress_cidr_blocks = ["0.0.0.0/0"]
+   ingress_rules       = ["http-80-tcp", "https-443"]
+    ingress_cidr_blocks = ["0.0.0.0/0"]
 
-   # egress_rules       = ["all-all"]
-   # egress_cidr_blocks = ["0.0.0.0/0"]
- # }
+    egress_rules       = ["all-all"]
+    egress_cidr_blocks = ["0.0.0.0/0"]
+  }
 
 resource "aws_security_group" "blog" {
   name        = "blog"
